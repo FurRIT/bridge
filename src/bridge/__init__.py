bb59a2dca83e86e5a6ba7cdf2684d3d20dcc44df
minkedalt <alt.gh@minkedup.xyz>
2025-09-22 23:31:42 -0400

    Added basic framework for bridge

--- src/bridge/__init__.py
+++ src/bridge/__init__.py
@@ -1,2 +1,29 @@
+"""
+Bridge to legacy server.
+"""
+
+import asyncio
+
+import playwright.async_api
+
+
+async def run() -> None:
+    """
+    Run Application.
+    """
+    play = await playwright.async_api.async_playwright().start()
+
+    browser = await play.firefox.launch(headless=False)
+    page = await browser.new_page()
+
+    await page.goto("https://playwright.dev")
+
+    await asyncio.sleep(30)
+    await browser.close()
+
+
 def main() -> None:
-    print("Hello from bridge!")
+    """
+    Parse Arguments & Run Main.
+    """
+    asyncio.run(run())
