ef6002137b623ae364fc0c8695d78b5cbf229ad6
minkedalt <alt.gh@minkedup.xyz>
2025-09-23 21:38:56 -0400

    Added function to check authorization

--- src/bridge/site/auth.py
+++ src/bridge/site/auth.py
@@ -20,3 +20,13 @@ async def i_login(
     await page.get_by_placeholder("FurRIT Username").fill(username)
     await page.get_by_placeholder("Password").fill(password)
     await page.locator(_LOGIN_BUTTON_CSS_PATH).click()
+
+
+async def check_auth(page: playwright.async_api.Page, host: str) -> bool:
+    """
+    Use the Page to check whether or not the Context a Page belongs to is
+    authorized.
+    """
+
+    await page.goto(f"https://{host}/events")
+    return not page.url.endswith("/login")
