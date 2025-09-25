3fb623987fcfa3a4f81a8719474a69aa00759c57
minkedalt <alt.gh@minkedup.xyz>
2025-09-25 10:09:03 -0400

    Moved event querying into its own function

--- src/bridge/__init__.py
+++ src/bridge/__init__.py
@@ -2,6 +2,7 @@
 Bridge to legacy server.
 """
 
+from typing import Sequence
 import sys
 import os.path
 import logging
@@ -12,17 +13,18 @@ import textwrap
 import playwright.async_api
 
 from bridge.config import ConfigParseError, Config, try_load_config
+from bridge.event import Event
 from bridge.site.auth import i_login, try_load_do_auth
 from bridge.site.event import i_extract_event_ids, i_extract_event
 
 
-async def run(config: Config) -> None:
+async def fetch_events(config: Config) -> Sequence[Event]:
     """
-    Run Application.
+    Fetch Events from `site.host`.
     """
     play = await playwright.async_api.async_playwright().start()
+    browser = await play.firefox.launch(headless=True)
 
-    browser = await play.firefox.launch(headless=False)
     context, page = await try_load_do_auth(
         browser,
         config.site.host,
@@ -31,8 +33,22 @@ async def run(config: Config) -> None:
         config.site.password,
     )
 
-    head, *_ = await i_extract_event_ids(page, config.site.host)
-    await i_extract_event(context, config.site.host, head)
+    logging.info("querying site for event ids")
+
+    raw_uids = await i_extract_event_ids(page, config.site.host)
+    uids = frozenset(raw_uids)
+
+    logging.info("found event ids %s", uids)
+
+    events = []
+    for uid in uids:
+        get_event_resp = await i_extract_event(context, config.site.host, uid)
+        if get_event_resp is None:
+            logging.warning("encountered error querying details of event id=%s", uid)
+            continue
+
+        event = Event.from_raw_event(get_event_resp["data"])
+        events.append(event)
 
     await page.close()
 
@@ -40,6 +56,16 @@ async def run(config: Config) -> None:
     await context.close()
 
     await browser.close()
+    await play.stop()
+
+    return events
+
+
+async def run(config: Config) -> None:
+    """
+    Run Application.
+    """
+    events = await fetch_events(config)
 
 
 def _error(msg: str) -> None:
